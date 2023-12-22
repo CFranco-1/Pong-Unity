2571a97bd4e9f4f0dfdf707f025602bb81e59adf
Cesar Franco <cesarfrancopro@gmail.com>
2023-12-22 17:53:43 -0600

    added functions to Player Paddle, should be working properly. will adjust values later

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -1,18 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public float speed = 10.0f;
+    protected Rigidbody2D _rigidbody;
+
+    private void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 }
--- Assets/Scripts/PlayerPaddle.cs
+++ Assets/Scripts/PlayerPaddle.cs
@@ -1,18 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerPaddle : MonoBehaviour
+public class PlayerPaddle : Paddle
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 _direction;
+
+// Function to identify which direction player paddle will move
+    private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+            _direction = Vector2.up;
+        } else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
+            _direction = Vector2.down;
+        } else {
+            _direction = Vector2.zero;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+// Makes the player paddle move at a consistent speed so it doesn't fluctuate  
+    private void FixedUpdate()
     {
-        
+        if (_direction.sqrMagnitude != 0) {
+            _rigidbody.AddForce(_direction * this.speed);
+        }
     }
 }
